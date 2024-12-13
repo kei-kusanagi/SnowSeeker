cfe0756dc76e20cc6294339f07df3b00dab578ce
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-13 13:37:09 -0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Juan Carlos Robledo Morales on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSeekerTests/SnowSeekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerTests.swift
+//  SnowSeekerTests
+//
+//  Created by Juan Carlos Robledo Morales on 13/12/24.
+//
+
+import Testing
+@testable import SnowSeeker
+
+struct SnowSeekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
