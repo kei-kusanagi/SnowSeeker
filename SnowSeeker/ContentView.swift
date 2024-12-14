43ce22423a04326f2994fe53559f2546c4e5334d
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-13 18:46:07 -0600

    Using groups as transparent layout containers

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,31 +7,65 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
+//struct ContentView: View {
+//    @State private var layoutVertically = false
+//
+//    var body: some View {
+//        Button {
+//            layoutVertically.toggle()
+//        } label: {
+//            if layoutVertically {
+//                VStack {
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
+//            }
+//        }
+//    }
+//}
+
 struct ContentView: View {
-    
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
 
     var body: some View {
-        Button("Tap Me") {
-            selectedUser = User()
-            isShowingUser = true
+//        if sizeClass == .compact {
+//            VStack(content: UserView.init)
+//        } else {
+//            HStack(content: UserView.init)
+//        }
 
+//        if horizontalSizeClass == .compact {
+//            VStack {
+//                UserView()
+//            }
+//        } else {
+//            HStack {
+//                UserView()
+//            }
+//        }
+        ViewThatFits {
+            Rectangle()
+                .frame(width: 500, height: 200)
+
+            Circle()
+                .frame(width: 200, height: 200)
         }
-//        .sheet(item: $selectedUser) { user in
-//            Text(user.id)
-//        }
-//        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
-//            Button(user.id) { }
-//        }
-                .sheet(item: $selectedUser) { user in
-                    Text(user.id)
-                        .presentationDetents([.medium, .large])
-                }
+
     }
 }
 
