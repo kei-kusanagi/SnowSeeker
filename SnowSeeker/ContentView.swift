d7325941f0c8ff0c7ea3a5aecb9c7b5e6f494cd9
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-13 18:50:09 -0600

    Making a SwiftUI view searchable

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,64 +8,38 @@
 import SwiftUI
 
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
-
 //struct ContentView: View {
-//    @State private var layoutVertically = false
+//    @State private var searchText = ""
 //
 //    var body: some View {
-//        Button {
-//            layoutVertically.toggle()
-//        } label: {
-//            if layoutVertically {
-//                VStack {
-//                    UserView()
-//                }
-//            } else {
-//                HStack {
-//                    UserView()
-//                }
-//            }
+//        NavigationStack {
+//            Text("Searching for \(searchText)")
+//                .searchable(text: $searchText, prompt: "Look for something")
+//                .navigationTitle("Searching")
 //        }
 //    }
 //}
 
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { $0.localizedStandardContains(searchText) }
+        }
+    }
 
     var body: some View {
-//        if sizeClass == .compact {
-//            VStack(content: UserView.init)
-//        } else {
-//            HStack(content: UserView.init)
-//        }
-
-//        if horizontalSizeClass == .compact {
-//            VStack {
-//                UserView()
-//            }
-//        } else {
-//            HStack {
-//                UserView()
-//            }
-//        }
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-
-            Circle()
-                .frame(width: 200, height: 200)
+        NavigationStack {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
         }
-
     }
 }
 
