c24a2b3b9d6f24944e3c576f069fbde91e52330d
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-18 14:10:12 -0600

    Letting the user mark favorites

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -11,6 +11,8 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
 
+    @Environment(Favorites.self) var favorites
+
     @State private var selectedFacility: Facility?
     @State private var showingFacility = false
     
@@ -60,6 +62,15 @@ struct ResortView: View {
                     .padding(.vertical)
                 }
                 .padding(.horizontal)
+                Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                    if favorites.contains(resort) {
+                        favorites.remove(resort)
+                    } else {
+                        favorites.add(resort)
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
@@ -70,11 +81,11 @@ struct ResortView: View {
         } message: { facility in
             Text(facility.description)
         }
-
     }
 }
 
 
 #Preview {
     ResortView(resort: .example)
+        .environment(Favorites())
 }
