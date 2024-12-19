ca3d59072719ff12e42b9e7c4daa2868338dec65
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-12-18 18:28:14 -0600

    Challenge 1
    Add a photo credit over the ResortView image. The data is already loaded from the JSON for this purpose, so you just need to make it look good in the UI.

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -22,9 +22,18 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                
+                ZStack(alignment: .bottomTrailing) {
+                    Image(decorative: resort.id)
+                        .resizable()
+                        .scaledToFit()
+
+                    Text(resort.imageCredit)
+                        .padding(10)
+                        .background(.black.opacity(0.7))
+                        .foregroundStyle(.white)
+                        .offset(x: -5, y: -5)
+                }
                 
                 HStack {
                     if horizontalSizeClass == .compact && dynamicTypeSize > .large {
